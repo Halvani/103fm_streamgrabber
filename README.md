9dee86958c8e74f4941084195f4ad3f44fa573af
Oren Halvani <xxx@yyy.com>
2022-02-02 12:43:38 +0100

    .

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # 103fm StreamGrabber
 This small script downloads episodes of arbitrary radio shows from the 103fm website: https://103fm.maariv.co.il/.
 
-Currently, four radio shows are predefined, which can be extended easily by simply selecting the desired radio show under: 
-https://103fm.maariv.co.il/programs
+Currently a dictionary of five radio shows is predefined, which can be extended easily. 
+For this, you first have to select your favorite radio show under https://103fm.maariv.co.il/programs 
+For example, the radio show of Didi Lokali ("דידי-לוקאלי") can be found under the URL: 
+https://103fm.maariv.co.il/program/%D7%93%D7%99%D7%93%D7%99-%D7%9C%D7%95%D7%A7%D7%90%D7%9C%D7%99-%D7%93%D7%99%D7%93%D7%99-%D7%94%D7%A8%D7%A8%D7%99.aspx
+Within this URL you must afterwards copy the URL of the complete epidodes ("תוכניות מלאות"): 
+https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=EG 
+This URL needs to be added to the predefined dictionary. 
 
-Have fun 😉
+To download the shows, all you have to do is specify the destination folder. After that you just need to run the script either from the command line or by double-clicking it in your desired file browser. 
+
+Enjoy listening!
 
 
 # Dependencies:
-Requests --> pip install requests <br>
-BeautifulSoup --> pip install beautifulsoup4 <br>
-Inspect --> The Python Standard Library: https://docs.python.org/3/library/inspect.html#module-inspect <br>
-Webbrowser --> The Python Standard Library: https://docs.python.org/3/library/webbrowser.html#module-webbrowser
+beautifulsoup4==4.10.0
+requests==2.27.1
+tqdm==4.62.3
--- radio_103fm_streamgrabber.py
+++ radio_103fm_streamgrabber.py
@@ -1,91 +1,114 @@
-import requests
-from bs4 import BeautifulSoup
+import os
 import inspect
+import requests
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+from datetime import datetime
 import webbrowser
 
-def var_name(var):
-    """ Gets the name of a given variable. """
+def name(var):
+    """ Gets the name of the given variable. """
     for fi in reversed(inspect.stack()):
         names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
-        if len(names) > 0:
-            return names[0]
+        if len(names) > 0: return names[0]
 
 
-def url2html(url):
+def download_html(url):
     response = requests.get(url)
     if response.status_code == 200:
         return str(response.content)
+    else:
+        print(response.raise_for_status())
 
 
-def download_url(url, filename):
-    response = requests.get(url)
-    #response = requests.get(url, headers={'Connection': 'close'})
+def download_episode(episode_url, filename):
+    response = requests.get(episode_url)
     if response.status_code == 200:
         with open(filename, 'wb') as file:
             file.write(response.content)
-        file.close()
     else:
-        print(f"Couldn't download: {url} --> Server status code: {str(response.status_code)}")
+        print(response.raise_for_status())
 
+
+def reformat_date(date_str):
+    date = date_str[3:][:6]
+    clean_date = datetime.strptime(date, '%d%m%y').strftime("%d.%m.%Y")
+    return clean_date
 
 
 def construct_episode_url4download(single_episode_url):
-    html = url2html(single_episode_url)
-    soup = BeautifulSoup(html, "lxml")
+    soup = BeautifulSoup(download_html(single_episode_url), "lxml")
     data_file = soup.find_all('div', class_='mouthjs-autoplay')[0]['data-file']
-    stream_download_url = f"https://awaod01.streamgates.net/103fm_aw/{data_file}.mp3" 
+    stream_download_url = f'https://awaod01.streamgates.net/103fm_aw/{data_file}.mp3'
     return stream_download_url
 
 
 def extract_episodes_urls(full_episodes_url):
-    ''' Starting from a given 103-FM radio show url: extract all episode URLs. '''
-    html = url2html(full_episodes_url)
+    ''' Given an 103-FM radio show url --> extract all episode URLs. '''
 
-    episode_indicator = "/programs/media.aspx?"
+    episode_indicator = '/programs/media.aspx?'
     episode_id = full_episodes_url[-3:]
-
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(download_html(full_episodes_url), 'html.parser')
     episodes_urls = []
+
     for a in soup.find_all('a', href=True):
         temp_url = a['href']
         if temp_url.startswith(episode_indicator) and temp_url.endswith(episode_id):
-            episodes_urls.append(f"https://103fm.maariv.co.il{temp_url}")
+            episodes_urls.append(f'https://103fm.maariv.co.il{temp_url}')
 
     episodes_download_urls = [construct_episode_url4download(episodes_url) for episodes_url in episodes_urls]
     return episodes_download_urls
 
 
-def download_all_streams(episodes, dest_path, open_download_folder=True):
-    for episode_id, url_complete_episodes in episodes.items():
+def download_all_streams(episodes, dest_folder='', create_nonexisting_dir=True, open_download_folder=False,
+                         pretty_print_filename=False, verbose=0):
+    if not os.path.exists(dest_folder) and create_nonexisting_dir:
+        os.mkdir(dest_folder)
+
+    for episode_id, url_complete_episodes in tqdm(episodes.items()):
         full_episodes_urls = extract_episodes_urls(url_complete_episodes)
 
         for full_episodes_url in full_episodes_urls:
-            filename = full_episodes_url[full_episodes_url.rfind("/") + 1:]
+            filename = full_episodes_url[full_episodes_url.rfind('/') + 1:]
+            full_episode_date = reformat_date(filename[:-3])
 
-            print(f"Downloading show-id: {episode_id} from url: {full_episodes_url}")
-            download_url(full_episodes_url, dest_path + "\\" + filename)
-            print('Done.')
+            if pretty_print_filename:
+                filename = f'{filename[:3]}_{full_episode_date}.mp3'
 
-    print('--> Finished all downloads.')
-    if open_download_folder: webbrowser.open(dest_path)
+            if verbose == 2:
+                print(f'\nDownloading stream [{episode_id}] --> date: {full_episode_date} from url:\n{full_episodes_url}')
 
+            if dest_folder == '':
+                download_episode(full_episodes_url, filename)
+            else:
+                download_episode(full_episodes_url, dest_folder + '\\' + filename)
+            if verbose == 2: print('Done.')
 
+    if verbose == 1:
+        print('--> Finished all downloads.')
 
-#---------------------------------------------------------------------------------------
-# Define episodes-urls: To add more lookup_ https://103fm.maariv.co.il/programs/ and add you respective show unter the menu: "תוכניות"
-#---------------------------------------------------------------------------------------
-zehavi   = "https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=EM"
-didi     = "https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=EG"
-caraso   = "https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=FF"
-iris_kol = "https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=M"
-
-episodes = dict()
-episodes.update({var_name(zehavi):zehavi})
-episodes.update({var_name(didi):didi})
-episodes.update({var_name(caraso):caraso})
-episodes.update({var_name(iris_kol):iris_kol})
-#---------------------------------------------------------------------------------------
+    if open_download_folder:
+        webbrowser.open(dest_folder)
 
 
-dest_path = r"ENTER_DESTINATION_PATH_HERE"
-download_all_streams(episodes, dest_path)
+if __name__ == '__main__':
+    # ---------------------------------------------------------------------------------------
+    # Define episodes-urls: To add more just lookup_ https://103fm.maariv.co.il/programs/ and
+    # add the respective show unter the menu: "תוכניות"
+    # ---------------------------------------------------------------------------------------
+    adam = 'https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=HEM'
+    zehavi = "https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=EM"
+    didi = "https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=EG"
+    caraso = "https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=FF"
+    iris_kol = "https://103fm.maariv.co.il/programs/complete_episodes.aspx?c41t4nzVQ=M"
+
+    episodes = dict()
+    episodes[name(adam)] = adam
+    episodes[name(zehavi)] = zehavi
+    episodes[name(didi)] = didi
+    episodes[name(caraso)] = caraso
+    episodes[name(iris_kol)] = iris_kol
+    # ---------------------------------------------------------------------------------------
+
+    dest_folder = r'YOUR_DESTINATION_FOLDER'
+    download_all_streams(episodes, dest_folder, pretty_print_filename=True, verbose=2)
